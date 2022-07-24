269ece47942c0244b44a9c3648a3887ac4fd3605
SlamChillz <mendyslam@gmail.com>
2022-07-24 02:42:12 +0000

    bubble sort

---
+++ 0-bubble_sort.c
@@ -0,0 +1,33 @@
+#include "sort.h"
+
+/**
+  * bubble_sort - sorts array in ascending order using bubble sort
+  * @array: array to be sorted
+  * @size: length of array
+  *
+  * Return: void
+  */
+void bubble_sort(int *array, size_t size)
+{
+	size_t in, out;
+	int socket;
+	bool bubbled;
+
+	for (out = 0; out < size - 1; out++)
+	{
+		bubbled = false;
+		for (in = 0; in < size - out - 1; in++)
+		{
+			if (array[in] > array[in + 1])
+			{
+				socket = array[in];
+				array[in] = array[in + 1];
+				array[in + 1] = socket;
+				print_array(array, size);
+				bubbled = true;
+			}
+		}
+		if (!bubbled)
+			break;
+	}
+}
